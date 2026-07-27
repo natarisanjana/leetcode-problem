973c03594f4056078db7cabc9233479e738f3a6d
natarisanjana <natarisanjana@gmail.com>
2026-07-27 12:37:54 +0530

    Add Solution class with toLowerCase method

---
+++ 709.to lower case.JAVA
@@ -0,0 +1,6 @@
+class Solution {
+    public String toLowerCase(String s) {
+        // Simple 1-liner relying on Java's built-in locale-aware string manipulation
+        return s.toLowerCase();
+    }
+}
